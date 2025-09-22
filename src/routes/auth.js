6e44e290362a4429b0721bfe856a3084e8a08c82
Zahidul Islam Zihad <soft.zihad@gmail.com>
2025-09-22 15:04:44 +0600

    Create Route for Auth

---
+++ src/routes/auth.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { login, register, me} from '../controllers/authController.js';
+
+const router = express.Router();
+
+// Auth routes
+router.post('/register', register);
+router.post('/login', login);
+router.get('/me', me);
+
+export default router;
