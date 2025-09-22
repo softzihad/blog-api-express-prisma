63fe5e4e400cfeb57ad37c968ae6710fe56ee743
Zahidul Islam Zihad <soft.zihad@gmail.com>
2025-09-22 16:07:19 +0600

    protected route using authMiddleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { login, register, me} from '../controllers/authController.js';
+import { login, register, me } from '../controllers/authController.js';
+import { authMiddleware }  from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Auth routes
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', me);
+router.get('/me', authMiddleware, me);
 
 export const authRouter = router;
